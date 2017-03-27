559f2b3971c002da9630000acfa3c7036aa383ac
C-Nug <cnug.pwnz@gmail.com>
2017-03-27 12:24:58 -0500

    put encrypted message in a textform with submit button

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = "Hello boobs"
         encryptedmessage = caesar.encrypt(message, 13)
-        self.response.write(encryptedmessage)
+
+        textarea = "<textarea>" + encryptedmessage + "</textarea>"
+        submit = "<input type='submit'>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+
+
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
